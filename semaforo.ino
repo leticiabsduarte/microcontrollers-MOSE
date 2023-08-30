ab9f6813b188c724a2eb10e9efdfe32f97fb0ca4
Letícia Duarte <leticiabsduarte@gmail.com>
2023-08-30 10:43:26 -0300

    Update semaforo.ino

--- semaforo.ino
+++ semaforo.ino
@@ -1,1 +1,21 @@
-v
+void setup() {
+  pinMode(7,OUTPUT);
+  pinMode(6,OUTPUT);
+  pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+pinMode(7,HIGH);
+delay(3000);
+pinMode(7,LOW);
+
+pinMode(6,HIGH);
+delay(3000);
+pinMode(6,LOW);
+
+pinMode(5,HIGH);
+delay(3000);
+pinMode(5,LOW);
+
+}
